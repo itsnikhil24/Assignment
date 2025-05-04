66aa9e566c6b4c09988ff9c4cd52e35f17f53056
Nikhil Sharma <nikhils.sharma2409@gmail.com>
2025-05-04 13:08:57 +0530

    fixed error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,39 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- 
+
+  images: {
+    remotePatterns: [
+      // Localhost (port 3000)
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
+      // pixel.com (HTTPS default port)
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      // example.com (the one you need!)
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
